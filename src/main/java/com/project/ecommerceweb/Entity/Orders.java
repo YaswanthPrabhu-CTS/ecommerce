c67d800564487bc7805cb805432f1808a453bd04
Yaswanth Prabhu <2503255@cognizant.com>
2026-06-02 10:02:39 +0530

    comment

--- src/main/java/com/project/ecommerceweb/Entity/Orders.java
+++ src/main/java/com/project/ecommerceweb/Entity/Orders.java
@@ -1,6 +1,5 @@
 package com.project.ecommerceweb.Entity;
 
-<<<<<<< HEAD
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -8,13 +7,6 @@ import java.util.List;
 
 import jakarta.persistence.*;
 import lombok.Data;
-=======
-import java.time.LocalDateTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
->>>>>>> varunchanges
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,8 +18,6 @@ public class Orders {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-<<<<<<< HEAD
-//    private Long userId;
 
     @ManyToOne(optional = false)
     private User user;
@@ -38,10 +28,5 @@ public class Orders {
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> items = new ArrayList<>();
-=======
-    private Long userId;
-    private double totalAmount;
-    private String status;
-    private LocalDateTime orderDate;
->>>>>>> varunchanges
+
 }
